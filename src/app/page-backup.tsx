6b65eb5e8ffa9601ed4173e10436005e529245bf
Timothy McDonald <>
2025-08-05 15:19:47 +1000

    Simplify homepage to debug 404 issue - basic version

---
+++ src/app/page-backup.tsx
@@ -0,0 +1,8 @@
+// Backup of original page
+import { content } from "@/lib/content";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Home() {
+  // ... rest of original content
+}
